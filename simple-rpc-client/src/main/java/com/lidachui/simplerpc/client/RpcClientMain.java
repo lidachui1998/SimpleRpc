811711baed0694ddcf0bc4c5857d31c335a07b13
lihuijie <huijie.li@greencloud.com.cn>
2024-08-19 23:23:29 +0800

    Initial commit

---
+++ simple-rpc-client/src/main/java/com/lidachui/simplerpc/client/RpcClientMain.java
@@ -0,0 +1,10 @@
+package com.lidachui.simplerpc.client;
+
+/**
+ * RpcClientMain
+ *
+ * @author: lihuijie
+ * @date: 2024/8/19 23:18
+ * @version: 1.0
+ */
+public class RpcClientMain {}
---
+++ simple-rpc-server/src/main/java/com/lidachui/simplerpc/server/RpcServerMain.java
@@ -0,0 +1,10 @@
+package com.lidachui.simplerpc.server;
+
+/**
+ * RpcServerMain
+ *
+ * @author: lihuijie
+ * @date: 2024/8/19 23:18
+ * @version: 1.0
+ */
+public class RpcServerMain {}
