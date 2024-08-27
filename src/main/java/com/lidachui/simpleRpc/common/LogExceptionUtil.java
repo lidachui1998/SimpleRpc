6e8091e6b7b921eb9123c9413778c992b2d54b79
lihuijie <huijie.li@greencloud.com.cn>
2024-08-27 21:50:52 +0800

    feat: 错误响应处理、日志打印处理

--- src/main/java/com/lidachui/simpleRpc/common/LogExceptionUtil.java
+++ src/main/java/com/lidachui/simpleRpc/common/LogExceptionUtil.java
@@ -19,11 +19,17 @@ public class LogExceptionUtil {
      * @return {@code String }
      */
     public static String getExceptionMessage(Throwable ex) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        ex.printStackTrace(pw);
-        pw.flush();
-        pw.close();
-        return sw.toString();
+        if (ex == null) {
+            return "No exception message provided.";
+        }
+
+        try (StringWriter sw = new StringWriter();
+             PrintWriter pw = new PrintWriter(sw)) {
+            ex.printStackTrace(pw);
+            return sw.toString();
+        } catch (Exception e) {
+            // 捕获并返回异常处理过程中发生的任何错误
+            return "Error while getting exception message: " + e.getMessage();
+        }
     }
 }
