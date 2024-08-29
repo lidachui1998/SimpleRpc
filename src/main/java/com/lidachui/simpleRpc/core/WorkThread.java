3925d4e0f246de7ed1b5a845651b24a3ed5e4d33
李辉杰 <huijie.li@greencloud.com.cn>
2024-08-29 16:58:59 +0800

    feat: 响应报错修复

--- src/main/java/com/lidachui/simpleRpc/core/WorkThread.java
+++ src/main/java/com/lidachui/simpleRpc/core/WorkThread.java
@@ -3,7 +3,6 @@ package com.lidachui.simpleRpc.core;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.Socket;
 import lombok.AllArgsConstructor;
